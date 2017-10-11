53b9dd51d0e31ea679888ed3ff5cf0cb2a9cbd1c
mharvy <32440398+mharvy@users.noreply.github.com>
2017-10-10 20:16:30 -0500

    Formatted better

--- ReadBot.py
+++ ReadBot.py
@@ -6,14 +6,25 @@ reddit = praw.Reddit('bot1')
 
 # Grabs quan amount of posts from sub subreddit, prints their titles, scores, and up/downs
 def show_new_posts(sub, quan):
+    
     subreddit = reddit.subreddit(sub)
     print("\n\n")
+    
     for submission in subreddit.new(limit=int(quan)):
+        
         print("Title: " + submission.title)
         print("Score: " + str(submission.score))
         print("Upvotes/Downvotes: " + str(submission.ups) + "/" + str(submission.downs) + "\n\n")
 
 
-a = input("Enter subreddit:\n>   ")
-b = input("Enter quantity of posts:\n>   ")
-show_new_posts(a, b)
+def main():
+    
+    a = input("Enter subreddit:\n>   ")
+    b = input("Enter quantity of posts:\n>   ")
+    show_new_posts(a, b)
+        
+        
+if __name__ == '__main__':
+    
+    main()
+    
